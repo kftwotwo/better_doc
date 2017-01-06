1956f6079d35ac0fae4b29b9b0775e612d461402
Kevin Finley <kftwotwo@gmail.com>
2017-01-06 13:53:04 -0800

    able to get names

--- js/doctor_info.js
+++ js/doctor_info.js
@@ -6,13 +6,15 @@ function Doctor() {
 Doctor.prototype.getDoctors = function(specaility) {
   $.get("https://api.betterdoctor.com/2016-03-01/doctors?query="+ specaility + "&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=" + apiKey)
   .then(function(result) {
-    $('#test').append("<li>"+ result.first_name + "</li>");
-    console.log(result);
+    result.data.forEach(function(docs){
+      $('#test').append("<li>"+ docs.profile.first_name + "</li>");
+    });
   })
   .fail(function(error){
    $('.showDiv').text(error.responseJSON.message);
     console.log("fail");
   });
 };
+
 
 exports.doctorModule = Doctor;

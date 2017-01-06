e71059f27a99fa9a0fb1f367f27ddf96863f60c4
Kevin Finley <kftwotwo@gmail.com>
2017-01-06 11:36:54 -0800

    GOT DATA kinda

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,12 +1,12 @@
 var Doctor = require('./../js/doctor_info.js').doctorModule;
 var apiKey = require('./../.env').apiKey;
 
+
 $(document).ready(function() {
   document.getElementById("doc-input").defaultValue = "toothache";
   var currentDoctorObject = new Doctor();
   $('#input-button').click(function(){
-    var specaility = $('#userInput').val();
-    console.log('clicked')
+    var specaility = $('#doc-input').val();
     $('#doc-input').val("");
     currentDoctorObject.getDoctors(specaility);
   });
--- js/doctor_info.js
+++ js/doctor_info.js
@@ -5,11 +5,10 @@ function Doctor() {
 }
 
 Doctor.prototype.getDoctors = function(specaility) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ specaility +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(result) {
-     $('#test').append('<h2>' + specaility + '</h2>' + "Doctors that specialize in your issue: " + result.conditions);
+  $.get("https://api.betterdoctor.com/2016-03-01/doctors?query="+ specaility + "&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=" + apiKey).then(function(result) {
+     $('#test').append("<li>"+ result.name + "</li>");
       console.log(result);
-    })
-   .fail(function(error){
+    }).fail(function(error){
      $('.showDiv').text(error.responseJSON.message);
       console.log("fail");
     });

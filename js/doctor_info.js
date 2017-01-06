143dbea87771aa9dab93e18e26df25827410d0f5
Kevin Finley <kftwotwo@gmail.com>
2017-01-06 14:06:58 -0800

    add divs for styling

--- js/doctor_info.js
+++ js/doctor_info.js
@@ -7,7 +7,7 @@ Doctor.prototype.getDoctors = function(specaility) {
   $.get("https://api.betterdoctor.com/2016-03-01/doctors?query="+ specaility + "&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=" + apiKey)
   .then(function(result) {
     result.data.forEach(function(docs){
-      $('#test').append("<li>"+ docs.profile.first_name + "</li>");
+      $('#test').append("<div class='thumbnail'>"+ "<div class='caption'><h3>" + docs.profile.first_name + "</h3>" + "<p><a href='#' class='btn btn-primary' role='button'>Contact</a>"  + "<img src='docs.profile.image_url'>"  + "</div>");
     });
   })
   .fail(function(error){
